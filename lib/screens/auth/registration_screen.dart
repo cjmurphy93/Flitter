c87369d8358bf50ed3345c6cce54fc63dd588abf
cjmurphy93 <cjmurph93@gmail.com>
2021-07-21 20:45:14 -0400

    building registration screen

--- lib/screens/auth/registration_screen.dart
+++ lib/screens/auth/registration_screen.dart
@@ -1,15 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
-
+  static String id = 'registration_screen';
 
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String? email;
+  String? password;
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('hello'));
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // Flexible(
+          //   child: Hero(
+          //     tag: 'logo',
+          //     child: Container(
+          //       height: 200.0,
+          //       child: Image.asset('images/logo.png'),
+          //     ),
+          //   ),
+          // ),
+          SizedBox(
+            height: 48.0,
+          ),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            textAlign: TextAlign.center,
+            onChanged: (value) {
+              email = value;
+            },
+            // decoration: kTextFieldDecoration.copyWith(
+            //   hintText: 'Enter your email',
+            // ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          TextField(
+            obscureText: true,
+            textAlign: TextAlign.center,
+            onChanged: (value) {
+              password = value;
+            },
+            // decoration: kTextFieldDecoration.copyWith(
+            //   hintText: 'Enter your password',
+            // ),
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          // RoundedButton(
+          //   title: 'Register',
+          //   onPressed: () async {
+          //     setState(() {
+          //       showSpinner = true;
+          //     });
+          //     try {
+          //       final newUser = await _auth.createUserWithEmailAndPassword(
+          //         email: email,
+          //         password: password,
+          //       );
+          //       if (newUser != null) {
+          //         Navigator.pushNamed(
+          //           context,
+          //           ChatScreen.id,
+          //         );
+          //       }
+          //     } catch (e) {
+          //       print(e);
+          //     } finally {
+          //       setState(() {
+          //         showSpinner = false;
+          //       });
+          //     }
+          //   },
+          //   color: Colors.blueAccent,
+          // ),
+        ],
+      ),
+    );
   }
 }
