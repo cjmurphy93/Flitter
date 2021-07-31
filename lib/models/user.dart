b000b149915972a876406d7eb23fc1428b8dd971
cjmurphy93 <cjmurph93@gmail.com>
2021-07-30 23:24:15 -0400

    firebase auth initiated and user model utilized

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,9 @@
 class UserModel {
   final String id;
-  final String email;
+  final String? email;
 
   UserModel({
     required this.id,
-    required this.email,
+     this.email,
   });
 }
--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -3,5 +3,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
+  FirebaseAuth auth = FirebaseAuth.instance;
 
+  UserModel? _userFromFirebaseUser(User user) {
+    return user != null
+        ? UserModel(
+            id: user.uid,
+            email: user.email,
+          )
+        : null;
+  }
 }
