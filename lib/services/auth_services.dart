ebad0d34e230a869b0465367fa6ffe3a92fb3170
cjmurphy93 <cjmurph93@gmail.com>
2021-07-31 22:07:32 -0400

    figuring out usermodel stream

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -5,12 +5,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class AuthService {
   FirebaseAuth auth = FirebaseAuth.instance;
 
-  UserModel? _userFromFirebaseUser(User user) {
+  UserModel _userFromFirebaseUser(User? user) {
     return user != null
         ? UserModel(
             id: user.uid,
             email: user.email,
           )
         : null;
+  }
+
+  Stream<UserModel> get user {
+    return auth.authStateChanges().map(_userFromFirebaseUser);
   }
 }
