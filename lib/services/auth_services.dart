19273966839adc4cb0ea1b80f3acd977ad46d06c
cjmurphy93 <cjmurph93@gmail.com>
2021-08-01 23:05:27 -0400

    set up sign up

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -5,7 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class AuthService {
   FirebaseAuth auth = FirebaseAuth.instance;
 
-  UserModel _userFromFirebaseUser(User? user) {
+  UserModel? _userFromFirebaseUser(User? user) {
     return user != null
         ? UserModel(
             id: user.uid,
@@ -14,7 +14,33 @@ class AuthService {
         : null;
   }
 
-  Stream<UserModel> get user {
+  Stream<UserModel?> get user {
     return auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
+  Future signUp(email, password) async {
+    try {
+      UserCredential user = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.user!.uid)
+          .set({
+        'name': email,
+        'email': email,
+      });
+      _userFromFirebaseUser(user.user);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        print('An account already exists for that email');
+      } else if (e.code == 'weak-password') {
+        print('Please use a stronger password');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
