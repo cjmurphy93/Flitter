2a37cdd66bf860e72f7042def08cb808fbecd6c0
cjmurphy93 <cjmurph93@gmail.com>
2021-08-03 23:41:32 -0400

    fixed nav in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flitter/screens/welcome_screen.dart';
 import 'package:flitter/screens/auth/registration_screen.dart';
 import 'package:flitter/screens/auth/login_screen.dart';
+import 'package:flitter/screens/feed_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +22,7 @@ class Flitter extends StatelessWidget {
         WelcomeScreen.id: (context) => WelcomeScreen(),
         LoginScreen.id: (context) => LoginScreen(),
         RegistrationScreen.id: (context) => RegistrationScreen(),
+        FeedScreen.id: (context) => FeedScreen(),
       },
     );
   }
