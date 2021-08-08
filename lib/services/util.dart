b942bd6a2fa19370ad1ce4621bb6987b602179e4
cjmurphy93 <cjmurph93@gmail.com>
2021-08-07 22:18:56 -0400

    made postlistfromsnapshot

--- lib/services/util.dart
+++ lib/services/util.dart
@@ -1,1 +1,21 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flitter/models/post.dart';
+
+class PostService {
+  List<PostModel> _postListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return PostModel(
+        id: doc.id,
+        text: doc['text'] ?? '',
+        creator: doc['creator'] ?? '',
+        retweet: doc['retweet'] ?? false,
+        numLikes: doc['numLikes'] ?? 0,
+        numRetweets: doc['numRetweets'] ?? 0,
+        originalId: doc['originalId'] ?? null,
+        timestamp: doc['timestamp'] ?? 0,
+        ref: doc.reference,
+      );
+    }).toList();
+  }
+}
